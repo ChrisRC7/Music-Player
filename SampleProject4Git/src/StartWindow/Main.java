86454d8314cd64b4e826a6a36b4e451fd88cce59
Jay Gatsby <103780732+Gaiseric@users.noreply.github.com>
2022-08-27 19:02:40 -0600

    Autenticación de usuario
    Se incluye un proceso de autenticación de usuario en Java Swing que necesita mejoras.

---
+++ SampleProject4Git/src/StartWindow/Main.java
@@ -0,0 +1,10 @@
+package StartWindow;
+
+public class Main {
+    //Main Method
+    public static void main(String[] args){
+        //Instantiate launchScreen for the app
+        Credentials launchPage = new Credentials();
+    }
+
+}
